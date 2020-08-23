d4db28748ec81117561d3b9fbbb928b0b0511fc6
939514820 <939514820@qq.com>
2020-08-23 18:04:28 +0800

    first commit

--- test/src/Test.java
+++ test/src/Test.java
@@ -1,5 +1,27 @@
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+
 public class Test {
+    static private final byte messgae[]={83,111,109,101,32,98,121,116,101,115,46};
     public static void main(String[] args) {
-        System.out.println();
+        try {
+            FileOutputStream fout=new FileOutputStream("test.txt");
+            FileChannel channel = fout.getChannel();
+            ByteBuffer buffer = ByteBuffer.allocate(1024);
+            for(int i=0;i<messgae.length;++i){
+               buffer.put(messgae[i]);
+            }
+            buffer.flip();
+            channel.write(buffer);
+            fout.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println("==========");
     }
 }
